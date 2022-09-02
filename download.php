f43c7db0d2b7e2713b9a826b852ca9b4b3736aa1
NicolasAili <nicolas31.busquet@laposte.net>
2022-09-02 03:52:38 +0200

    initial commit

---
+++ download.php
@@ -0,0 +1,300 @@
+<?php
+$type = $_POST['type'];
+$format = $_POST['format'];
+$title = $_POST['title'];
+$rename = $_POST['rename'];
+$ordre = $_POST['ordre'];
+
+if(sizeof($title)>1)
+{
+    if($rename)
+    {
+        mkdir( $rename );
+        $renamedir = $rename;
+    }
+    else
+    {
+        mkdir( 'contenu' );
+        $renamedir = 'contenu';
+    }
+}
+else
+{
+    if($rename)
+    {
+        ?><a href="url.php"> retour </a><br><?php
+        exit('impossible d\'avoir un renommage si un seul lien');
+    }
+}
+
+switch ($type) {
+    case 'mptrois':
+        for ($i=0; $i < sizeof($title); $i++) 
+        {
+            echo "mp3";
+            if(sizeof($title)>1)
+            {
+                if ($format == 'best') 
+                {
+                    exec('cd ' . $renamedir . '&& yt-dlp -x ' . $title[$i], $output, $retval); //telecharge uniquement l'audio
+                }
+                else
+                {
+                    exec('cd ' . $renamedir . '&& yt-dlp -x --audio-format ' . $format . ' ' . $title[$i], $output, $retval); //telecharge uniquement l'audio
+                }
+            }
+            else
+            {
+                if ($format == 'best') 
+                {
+                    exec('yt-dlp -x ' . $title[$i], $output, $retval); //telecharge uniquement l'audio
+                }
+                else
+                {
+                    exec('yt-dlp -x --audio-format ' . $format . ' ' . $title[$i], $output, $retval); //telecharge uniquement l'audio
+                }
+            }
+            if($i == 0)
+            {
+                $slice = 5;
+            }
+            else
+            {
+                $slice = $slice + 7;
+            }
+
+            $input = array_slice($output, $slice, 1);  //récupère la partie de la réponse à la commande où se trouve le nom du fichier
+            $rest = implode("','",$input); //la convertit en une chaîne
+            $restarr[$i] = substr($rest, 28); //récupère uniquement le nom du fichier
+            $restarrcp = $restarr[$i]; //on garde le nom original (nom du fichier)
+            $find = 0; //0 = 3 caractères pour l'extension, 1=4, 2=6
+            $pos = strpos($restarr[$i], 'flac');
+            if ($pos === false) 
+            {
+                $pos = strpos($restarr[$i], 'opus');
+                if ($pos === false) 
+                {
+                    $pos = strpos($restarr[$i], 'alac');
+                    if ($pos === false) 
+                    {
+                        $pos = strpos($restarr[$i], 'vorbis');
+                        if ($pos === true) 
+                        {
+                            $find = 2;
+                        }
+                    }
+                    else
+                    {
+                        $find = 1;
+                    }
+                }
+                else
+                {
+                    $find = 1;
+                }
+            }
+            else
+            {
+                $find = 1;
+            }
+            switch ($find) 
+            {
+                case 0:
+                    $extension = substr($restarr[$i], -4); //récupère l'extension du fichier
+                    $restarr[$i] = substr($restarr[$i], 0, -18); //supprime les chaînes de caractère générées automatiquement par le logiciel
+                    break; 
+                case 1:
+                    $extension = substr($restarr[$i], -5); //récupère l'extension du fichier
+                    $restarr[$i] = substr($restarr[$i], 0, -19); //supprime les chaînes de caractère générées automatiquement par le logiciel
+                    break; 
+                case 2:
+                    $extension = substr($restarr[$i], -7); //récupère l'extension du fichier
+                    $restarr[$i] = substr($restarr[$i], 0, -21); //supprime les chaînes de caractère générées automatiquement par le logiciel
+                    break; 
+                default:
+                    echo "find inconnu"; //erreur
+                break;
+            }
+            
+            $restarr[$i] = $restarr[$i] . $extension; //rajoute l'extension
+            
+            if(sizeof($title)>1 && isset($ordre)) //si plus d'un fichier et qu'on veut conserver l'ordre
+            {
+                if($i < 10)
+                {
+                    $j = '0' . strval($i); //pour les fichier 1 à 9 on met 00,01,02...
+                }
+                else
+                {
+                    $j = strval($i); //sinon juste 10,11,12...
+                }
+                $restarr[$i] = $j . ' - ' . $restarr[$i]; //on met au format "numero - nomfichier"
+
+                rename ($renamedir . '/' . $restarrcp, $renamedir . '/' . $restarr[$i]); //on renomme
+            }
+            elseif(sizeof($title)==1)  
+            {
+                rename ($restarrcp, $restarr[$i]); //on renomme
+            }
+        }
+        break;
+    case 'mpquatre':
+        for ($i=0; $i < sizeof($title); $i++) 
+        { 
+            $find = 0;
+            if(sizeof($title)>1)
+            {
+                if ($format == 'best') 
+                {
+                    exec('cd ' . $renamedir . '&& yt-dlp ' . $title[$i], $output, $retval); //telecharge la video (et l'audio)
+                }
+                else
+                {
+                    exec('cd ' . $renamedir . '&& yt-dlp -f ' . $format . ' ' . $title[$i], $output, $retval); //telecharge la video (et l'audio)
+                }
+            }
+            else
+            {
+                if ($format == 'best') 
+                {
+                    exec('yt-dlp ' . $title[$i], $output, $retval); //telecharge la video (et l'audio)
+                }
+                else
+                {
+                    exec('yt-dlp -f ' . $format . ' ' . $title[$i], $output, $retval); //telecharge la video (et l'audio)
+                }
+            }
+
+            if ($format == 'best')
+            {
+                if($i == 0)
+                {
+                    $slice = 7;
+                }
+                else
+                {
+                    $slice = $slice + 10;
+                }
+                $input = array_slice($output, $slice, 1);  //récupère la partie de la réponse à la commande où se trouve le nom du fichier
+                $rest = implode("','",$input); //la convertit en une chaîne
+                $restarr[$i] = substr($rest, 31, -1); //récupère uniquement le nom du fichier
+                $restarrcp = $restarr[$i]; //on garde le nom original (nom du fichier)
+                $find = 0; //0 = 3 caractères pour l'extension, 1=4, 2=6*/
+            }
+            else
+            {
+                if($i == 0)
+                {
+                    $slice = 3;
+                }
+                else
+                {
+                    $slice = $slice + 5;
+                }
+                $input = array_slice($output, $slice, 1);  //récupère la partie de la réponse à la commande où se trouve le nom du fichier
+                $rest = implode("','",$input); //la convertit en une chaîne
+                $restarr[$i] = substr($rest, 24); //récupère uniquement le nom du fichier
+                $restarrcp = $restarr[$i]; //on garde le nom original (nom du fichier)
+                $find = 0; //0 = 3 caractères pour l'extension, 1=4 */
+            }
+
+            $pos = strpos($restarr[$i], 'webm');
+            if ($pos === false) {
+                $find = 0;
+            } else {
+                $find = 1;
+            }
+            
+            switch ($find) 
+            {
+                case 0:
+                    $extension = substr($restarr[$i], -4); //récupère l'extension du fichier
+                    $restarr[$i] = substr($restarr[$i], 0, -18); //supprime les chaînes de caractère générées automatiquement par le logiciel
+                    break; 
+                case 1:
+                    $extension = substr($restarr[$i], -5); //récupère l'extension du fichier
+                    $restarr[$i] = substr($restarr[$i], 0, -19); //supprime les chaînes de caractère générées automatiquement par le logiciel
+                    break;
+                default:
+                    echo "find inconnu"; //erreur
+                break;
+            }
+
+            $restarr[$i] = $restarr[$i] . $extension; //rajoute l'extension
+            
+            if(sizeof($title)>1 && isset($ordre)) //si plus d'un fichier et qu'on veut conserver l'ordre
+            {
+                if($i < 10)
+                {
+                    $j = '0' . strval($i); //pour les fichier 1 à 9 on met 00,01,02...
+                }
+                else
+                {
+                    $j = strval($i); //sinon juste 10,11,12...
+                }
+                $restarr[$i] = $j . ' - ' . $restarr[$i]; //on met au format "numero - nomfichier"
+
+                rename ($renamedir . '/' . $restarrcp, $renamedir . '/' . $restarr[$i]); //on renomme
+            }
+            elseif(sizeof($title)==1)  
+            {
+                rename ($restarrcp, $restarr[$i]); //on renomme
+            }
+        }
+        break;
+    default:
+        echo "erreur : aucun bouton n'a été coché"; //erreur
+        break;
+}
+
+if(sizeof($title)>1)
+{
+    if($rename)
+    {
+        exec('tar -cvf ' . $rename . '.tar ' . $rename); //crée l'archive renommée par l'utilisateur
+        $rest = $rename . '.tar';
+    }
+    else
+    {
+        exec('tar -cvf content.tar contenu'); //crée l'archive par défaut
+        $rest = 'content.tar';
+    }
+}
+else
+{
+    $rest = $restarr[0];
+}
+
+
+if (file_exists($rest)) { //télécharge le fichier
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($rest).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($rest));
+    readfile($rest);
+}
+else
+{
+    echo "erreur, un bug est apparu, pas de chance :/";
+}
+
+$rest = str_replace(" ", "\ ", $rest); //supprime les espaces car ça fait buguer la suppression
+
+exec('yes | rm ' . $rest); //supprime le fichier
+
+if(sizeof($title)>1)
+{
+    if($rename)
+    {
+        exec('yes | rm -r ' . $rename); //supprime les fichiers
+    }
+    else
+    {
+        exec('yes | rm -r contenu'); //supprime les fichiers
+    }
+}
+?> 
+<a href="url.php"> retour </a>
---
+++ url.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <style> 
+            #btn-back-to-top 
+            {
+                position: fixed;
+                bottom: 20px;
+                right: 20px;
+                display: none;
+            }
+        </style>
+    </head>
+    <body>
+        <button
+            type="button"
+            class="btn btn-danger btn-floating btn-lg"
+            id="btn-back-to-top"
+            >
+        <i class="fas fa-arrow-up"></i>
+        </button>
+        <h1> Outil de telechargement de vidéos youtube</h1>
+        <form action="download.php" method="post" style="margin-top: 15px; margin-left: 15px;">
+            <div id="inputFormRow">
+                <div class="input-group mb-3">
+                    <input type="text" name="title[]" class="form-control m-input" placeholder="Entrer URL" autocomplete="off" required>
+                </div>
+            </div>
+            <button id="addRow" type="button" class="btn btn-info">Ajouter un lien</button>
+            <div id="newRow"></div>
+            <div>
+                <input type="radio" id="mptrois" name="type" value="mptrois"
+                     checked>
+                <label for="mptrois">Audio</label>
+                <div id="formataudio" style="margin-left: 50px;">
+                    <div id="xx">
+                        <legend>Format audio</legend>
+                        <input type="radio" id="firstaud" name="format" value="best"
+                         checked>
+                        <label for="format">par défaut (le meilleur possible)</label>
+                        <input type="radio" id="formataud" name="format" value="aac">
+                        <label for="mptrois">aac</label>
+                        <input type="radio" id="formataud" name="format" value="flac">
+                        <label for="mptrois">flac</label>
+                        <input type="radio" id="formataud" name="format" value="mp3">
+                        <label for="mptrois">mp3</label>
+                        <input type="radio" id="formataud" name="format" value="m4a">
+                        <label for="mptrois">m4a</label>
+                        <input type="radio" id="formataud" name="format" value="opus">
+                        <label for="mptrois">opus</label>
+                        <input type="radio" id="formataud" name="format" value="vorbis">
+                        <label for="mptrois">vorbis</label>
+                        <input type="radio" id="formataud" name="format" value="wav">
+                        <label for="mptrois">wav</label>
+                        <input type="radio" id="formataud" name="format" value="alac">
+                        <label for="mptrois">alac</label>
+                    </div>
+                </div>
+            </div>
+            <div>
+              <input type="radio" id="mpquatre" name="type" value="mpquatre">
+              <label for="mpquatre">Video</label>
+              <div id="formatvideo" style="visibility: hidden; display: none;">
+                    <div id="xy">
+                        <legend>Format video</legend>
+                        <input type="radio" id="firstvid" name="format" value="best">
+                        <label for="format">par défaut (le meilleur possible)</label>
+                        <input type="radio" id="format" name="format" value="3gp">
+                        <label for="mptrois">3gp</label>
+                        <input type="radio" id="format" name="format" value="aac">
+                        <label for="mptrois">aac</label>
+                        <input type="radio" id="format" name="format" value="flv">
+                        <label for="mptrois">flv</label>
+                        <input type="radio" id="format" name="format" value="mp4">
+                        <label for="mptrois">mp4</label>
+                        <input type="radio" id="format" name="format" value="ogg">
+                        <label for="mptrois">ogg</label>
+                        <input type="radio" id="format" name="format" value="webm">
+                        <label for="mptrois">webm</label>
+                    </div>
+                </div>
+            </div>
+            <h4> Renommez si vous le souhaitez le dossier et l'archive qui contiendront vos téléchargements (uniquement si plusieurs liens) </h4>
+            <input type="text" name="rename" class="form-control m-input" placeholder="renommez" autocomplete="off">
+            <fieldset>
+                <legend>Conserver l'ordre des liens lors du téléchargement ? (uniquement si plusieurs liens)</legend>
+                <div>
+                    <input type="checkbox" id="ordre" name="ordre" checked>
+                    <label for="ordre">Coché pour oui, décoché pour non</label>
+                </div>
+            </fieldset>
+            <input type="submit" name="submit" value="TELECHARGER" style="background-color: #1a53ff; color: white; border-radius: 6px; padding: 5px; padding-left: 12px; padding-right: 12px; cursor: pointer;">
+        </form>
+        <input type="button" name="reinit" id="reinit" value="REINITIALISER" style="margin-left: 15px; background-color: #f90d0d; color: white; border-radius: 6px; padding: 5px; padding-left: 12px; padding-right: 12px; cursor: pointer;">
+        <h3 style="margin-left: 2%; margin-top: 50px;"> A noter </h3>
+        <div>
+            <p style="width: 80%; margin-left: 2%;"> 
+                Si une erreur apparaît ou que un ou plusieurs fichiers sont manquants, cela est très probablement dû au fait que le ou les fichiers souhaités n'est ou ne sont pas disponibles dans votre pays, ou bien que le format souhaité n'est pas disponible.
+            </p>
+        </div>
+    </body>
+</html>
+
+
+<script type="text/javascript">
+    // add row
+    $("#addRow").click(function () {
+        var html = '';
+        html += '<div id="inputFormRow">';
+        html += '<div class="input-group mb-3">';
+        html += '<input type="text" name="title[]" class="form-control m-input" placeholder="Entrer URL" autocomplete="off">';
+        html += '<div class="input-group-append">';
+        
+        html += '<button id="removeRow" type="button" class="btn btn-danger">Supprimer</button>';
+        html += '</div>';
+        html += '</div>';
+
+        $('#newRow').append(html);
+    });
+
+    // remove row
+    $(document).on('click', '#removeRow', function () {
+        $(this).closest('#inputFormRow').remove();
+    });
+
+    $("#mptrois").click(function () 
+    {
+        $("#formatvideo").css('visibility', 'hidden');
+        $("#formatvideo").css('display', 'none');
+        $("#formataudio").css('visibility', 'visible');
+        $("#formataudio").css('display', 'contents');
+        $("#xx").css('margin-left', '50px');
+        $("#firstvid").prop('checked', false);
+        $("#firstaud").prop('checked', true);   
+    });
+    $("#mpquatre").click(function () 
+    {
+        $("#formataudio").css('visibility', 'hidden');
+        $("#formataudio").css('display', 'none');
+        $("#formatvideo").css('visibility', 'visible');
+        $("#formatvideo").css('display', 'contents');
+        $("#xy").css('margin-left', '50px');
+        $("#firstaud").prop('checked', false);
+        $("#firstvid").prop('checked', true);
+    });
+
+    $("#reinit").click(function () {
+        $('#inputFormRow').remove();
+    });
+
+
+    //Get the button
+    let mybutton = document.getElementById("btn-back-to-top");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+    scrollFunction();
+    };
+
+    function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
+    function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+</script>
